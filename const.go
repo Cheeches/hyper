1a7c2c5916ce946b5ce99d50c163cfc5e94ee5f9
Thorsten Strauss <thorsten.strauss@voestalpine.com>
2018-01-24 07:30:46 +0100

    added parameter types

--- const.go
+++ const.go
@@ -1,10 +1,48 @@
 package hyper
 
 const (
-	// TypeHidden is for hidden parameters
-	TypeHidden = "hidden"
 	// TypeText is used for parameters of type text/string
 	TypeText = "text"
+	// TypeHidden is for hidden parameters
+	TypeHidden = "hidden"
+	// TypeButton is used for parameters of type button
+	TypeButton = "button"
+	// TypeCheckbox is used for parameters of type checkbox
+	TypeCheckbox = "checkbox"
+	// TypeColor is used for parameters of type color picker
+	TypeColor = "color"
+	// TypeDate is used for parameters of type date
+	TypeDate = "date"
+	// TypeDatetime is used for parameters of type date and time
+	TypeDatetime = "datetime"
+	// TypeEmail is used for parameters of type e-mail address
+	TypeEmail = "email"
+	// TypeImage is used for parameters of type image
+	TypeImage = "image"
+	// TypeMonth is used for parameters of type month
+	TypeMonth = "month"
+	// TypeNumber is used for parameters of type number
+	TypeNumber = "number"
+	// TypePassword is used for parameters of type password
+	TypePassword = "password"
+	// TypeRadio is used for parameters of type radio button
+	TypeRadio = "radio"
+	// TypeRange is used for parameters of type range
+	TypeRange = "range"
+	// TypeReset is used for parameters of type reset
+	TypeReset = "reset"
+	// TypeSearch is used for parameters of type search
+	TypeSearch = "search"
+	// TypeSubmit is used for parameters of type submit
+	TypeSubmit = "submit"
+	// TypeTel is used for parameters of type telephone number
+	TypeTel = "tel"
+	// TypeTime is used for parameters of type time
+	TypeTime = "time"
+	// TypeURL is used for parameters of type url
+	TypeURL = "url"
+	// TypeWeek is used for parameters of type week
+	TypeWeek = "week"
 )
 
 const (
